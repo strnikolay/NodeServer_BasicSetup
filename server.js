1daddae963f214b0d6782818534a28c5d0a4cb0a
Проходная <nikolayromanchuk88@gmail.com>
2023-11-17 23:53:54 +0300

    17_11_23

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require('dotenv').config()
 
-const DBConect = require('./db_connect')
+const DBConect = require('./Mongodb_connect')
 const app = require('./app.js')
 const httpPort = process.env.PORT || 5000;
 
 
 (async () => {
     try {
+
+    /*Подключение Баз Данных
+     * MongoDB*/    
     await DBConect()
+    /**Или PastgreSQL */    
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });    
+
     app.listen(httpPort, () => console.log(`Server has been started on ${httpPort}`))
     } catch (e) {
         console.log(e);
