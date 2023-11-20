92cb93b34664d0b9a8a71a481f99ba66f2cd0989
Проходная <nikolayromanchuk88@gmail.com>
2023-11-20 23:40:26 +0300

    20.11.23

--- controllers/auth.js
+++ controllers/auth.js
@@ -94,3 +94,35 @@ module.exports.registration = async function(req, res){
 
     
 }
+
+async logout(req, res, next) {
+    try {
+        const {refreshToken} = req.cookies;
+        const token = await userService.logout(refreshToken);
+        res.clearCookie('refreshToken');
+        return res.json(token);
+    } catch (e) {
+        next(e);
+    }
+}
+
+async activate(req, res, next) {
+    try {
+        const activationLink = req.params.link;
+        await userService.activate(activationLink);
+        return res.redirect(process.env.CLIENT_URL);
+    } catch (e) {
+        next(e);
+    }
+}
+
+async refresh(req, res, next) {
+    try {
+        const {refreshToken} = req.cookies;
+        const userData = await userService.refresh(refreshToken);
+        res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+        return res.json(userData);
+    } catch (e) {
+        next(e);
+    }
+}
